73637a785eebfbdbf9f758f72bf7ec2047504f87
Ben-Malik <benmaliktchamalam@gmail.com>
2020-04-29 18:11:23 +0300

    Update mde on final.py

--- HW2/final.py
+++ HW2/final.py
@@ -1,8 +1,29 @@
 from nltk import pos_tag,word_tokenize
 article = open("love.txt", "r") 
-abstract_file = open("100-400.txt", "r")
-concrete_file = open("400-700.txt", "r")
 
+
+def isWordConcrete(word): 
+    abstract_file = open("100-400.txt", "r")
+    concrete_file = open("400-700.txt", "r")
+    is_concrete = False
+    is_word_found = False
+
+    for line in abstract_file.readlines():
+        current = line.split("\t")
+
+        if (word.lower() == current[0].split(" ")[0].lower()):
+            is_word_found = True
+            break
+    if not is_word_found:
+        for line in concrete_file.readlines():
+            current = line.split("\t")
+            
+            if (word.lower() == current[0].split(" ")[0].lower()):
+                is_concrete = True
+                is_word_found =True
+                break
+    return is_concrete
+    
 def getWordSubjectiveness(word): 
     subsective_file = open("subjectives.tff", "r")
     isWordWeak = False
@@ -61,6 +82,8 @@ class Word:
         self.is_after_2_negative = is_after_2_negative
         self.is_weak = is_weak
         self.is_strong = is_strong
+        self.is_concrete = isWordConcrete(w)
+        
     
     def toString(self): 
         return self.before_1_pos + " <-- " + self.before_2_pos + " <- " + self.w + " - " + self.w_pos + ' -> ' + self.after_1_pos + " --> " + self.after_2_pos
@@ -70,17 +93,40 @@ class Word:
 
     def subjectivenessStringify(self):
         return self.is_weak, self.is_strong
+
+    def equals(self, word):
+        return word.is_strong == self.is_strong and word.is_weak == self.is_weak and word.is_positive == self.is_positive and word.is_negative == self.is_negative and word.is_before_1_positive == self.is_before_1_positive and word.is_before_1_negative == self.is_before_1_negative and word.is_before_2_positive == self.is_before_2_positive and word.is_before_2_negative == self.is_before_2_negative and word.is_after_1_positive == self.is_after_1_positive and word.is_after_1_negative == self.is_after_1_negative and word.is_after_2_positive == self.is_after_2_positive and word.is_after_2_negative == self.is_after_2_negative and word.w_pos == self.w_pos and word.before_1_pos == self.before_1_pos and word.before_2_pos == self.before_2_pos and word.after_1_pos == self.after_1_pos and word.after_2_post == self.after_2_pos
+
+class Group: 
+    def __init__(self, words):
+        self.group = list()
+        self.groupAbstactness = 0.0
+        self.groupConcreteness = 0.0
+
+        for word in words:
+            self.group.append(word)
+    
+    def __init__(self):
+        self.group = list()
+    
+    def addGroup(self, word):
+        self.group.append(word)
+
 class Sentence:
     def __init__(self, words):
         self.sentence = list()
-        
+        self.groups = list()
         for word in words:
             self.sentence.append(word)
     def __init__(self):
         self.sentence = list()
+        self.groups = list()
 
     def addWord(self, word):
         self.sentence.append(word)
+
+    def addGroup(self, group):
+        self.groups.append(group)
 
     def toString(self): 
         for word in self.sentence:
@@ -99,156 +145,156 @@ for line in article.readlines():
         output.append(word)
     sentences.append(output)
 
+def parseSentences():
+    #Pos for each word
+    for i in range(len(sentences)):
+        sentence = Sentence()
+        for j in range(len(sentences[i])):
+            before_1 = "No Before 1"
+            before_2 = "No Before 2"
+            after_1 = "No After 1"
+            after_2 = "No After 2"
+            word_before_1 = ""
+            word_before_2 = ""
+            word_after_1 = ""
+            word_after_2 = ""
+            subjectiveness = getWordSubjectiveness(sentences[i][j])
+            is_weak = subjectiveness[1]
+            is_strong = subjectiveness[0]
+            output = list()
+            word = pos_tag(word_tokenize(sentences[i][j]))[0][1]
+            if (i == 0):
+                if (j == 0):
+                    after_1 = pos_tag(word_tokenize(sentences[i][j+1]))[0][1]
+                    after_2 = pos_tag(word_tokenize(sentences[i][j+2]))[0][1]
+                    word_after_1 = sentences[i][j+1]
+                    word_after_2 = sentences[i][j+2]
+                elif (j == 1):
+                    after_1 = pos_tag(word_tokenize(sentences[i][j+1]))[0][1]
+                    after_2 = pos_tag(word_tokenize(sentences[i][j+2]))[0][1]
+                    before_1 = pos_tag(word_tokenize(sentences[i][0]))[0][1]
 
-#Pos for each word
-for i in range(len(sentences)):
-    sentence = Sentence()
-    for j in range(len(sentences[i])):
-        before_1 = "No Before 1"
-        before_2 = "No Before 2"
-        after_1 = "No After 1"
-        after_2 = "No After 2"
-        word_before_1 = ""
-        word_before_2 = ""
-        word_after_1 = ""
-        word_after_2 = ""
-        subjectiveness = getWordSubjectiveness(sentences[i][j])
-        is_weak = subjectiveness[1]
-        is_strong = subjectiveness[0]
-        output = list()
-        word = pos_tag(word_tokenize(sentences[i][j]))[0][1]
-        if (i == 0):
-            if (j == 0):
-                after_1 = pos_tag(word_tokenize(sentences[i][j+1]))[0][1]
-                after_2 = pos_tag(word_tokenize(sentences[i][j+2]))[0][1]
-                word_after_1 = sentences[i][j+1]
-                word_after_2 = sentences[i][j+2]
-            elif (j == 1):
-                after_1 = pos_tag(word_tokenize(sentences[i][j+1]))[0][1]
-                after_2 = pos_tag(word_tokenize(sentences[i][j+2]))[0][1]
-                before_1 = pos_tag(word_tokenize(sentences[i][0]))[0][1]
+                    word_after_1 = sentences[i][j+1]
+                    word_after_2 = sentences[i][j+1]
+                    word_before_1 = sentences[i][0]
+                elif (j == len(sentences[i])-1):
+                    
+                    after_1 = pos_tag(word_tokenize(sentences[i+1][0]))[0][1]
+                    after_2 = pos_tag(word_tokenize(sentences[i+1][1]))[0][1]
+                    before_1 = pos_tag(word_tokenize(sentences[i][j-1]))[0][1]
+                    before_2 = pos_tag(word_tokenize(sentences[i][j-2]))[0][1]
 
-                word_after_1 = sentences[i][j+1]
-                word_after_2 = sentences[i][j+1]
-                word_before_1 = sentences[i][0]
-            elif (j == len(sentences[i])-1):
-                
-                after_1 = pos_tag(word_tokenize(sentences[i+1][0]))[0][1]
-                after_2 = pos_tag(word_tokenize(sentences[i+1][1]))[0][1]
+                    
+                    word_after_1 = sentences[i+1][0]
+                    word_after_2 = sentences[i+1][1]
+                    word_before_1 = sentences[i][j-1]
+                    word_before_2 = sentences[i][j-2]
+                elif (j == len(sentences[i])-2):
+                    after_1 = pos_tag(word_tokenize(sentences[i][j+1]))[0][1]
+                    after_2 = pos_tag(word_tokenize(sentences[i+1][0]))[0][1]
+
+                    before_1 = pos_tag(word_tokenize(sentences[i][j-1]))[0][1]
+                    before_2 = pos_tag(word_tokenize(sentences[i][j-2]))[0][1]
+
+                    word_after_1 = sentences[i][j+1]
+                    word_after_2 = sentences[i+1][0]
+                    word_before_1 = sentences[i][j-1]
+                    word_before_2 = sentences[i][j-2]
+                else: 
+                    after_1 = pos_tag(word_tokenize(sentences[i][j+1]))[0][1]
+                    after_2 = pos_tag(word_tokenize(sentences[i][j+2]))[0][1]
+                    before_1 = pos_tag(word_tokenize(sentences[i][j-1]))[0][1]
+                    before_2 = pos_tag(word_tokenize(sentences[i][j-2]))[0][1]   
+
+                    word_after_1 = sentences[i][j+1]
+                    word_after_2 = sentences[i][j+2]
+                    word_before_1 = sentences[i][j-1]
+                    word_before_2 = sentences[i][j-2]
+            elif (i == len(sentences)-1 and j == len(sentences[i])-1):
                 before_1 = pos_tag(word_tokenize(sentences[i][j-1]))[0][1]
                 before_2 = pos_tag(word_tokenize(sentences[i][j-2]))[0][1]
 
-                
-                word_after_1 = sentences[i+1][0]
-                word_after_2 = sentences[i+1][1]
                 word_before_1 = sentences[i][j-1]
                 word_before_2 = sentences[i][j-2]
-            elif (j == len(sentences[i])-2):
-                after_1 = pos_tag(word_tokenize(sentences[i][j+1]))[0][1]
-                after_2 = pos_tag(word_tokenize(sentences[i+1][0]))[0][1]
-
+            elif (i == len(sentences)-1 and j == len(sentences[i])-2):
                 before_1 = pos_tag(word_tokenize(sentences[i][j-1]))[0][1]
                 before_2 = pos_tag(word_tokenize(sentences[i][j-2]))[0][1]
+                after_1 = pos_tag(word_tokenize(sentences[i][j+1]))[0][1]
 
-                word_after_1 = sentences[i][j+1]
-                word_after_2 = sentences[i+1][0]
                 word_before_1 = sentences[i][j-1]
                 word_before_2 = sentences[i][j-2]
+                word_after_1 = sentences[i][j+1]
             else: 
-                after_1 = pos_tag(word_tokenize(sentences[i][j+1]))[0][1]
-                after_2 = pos_tag(word_tokenize(sentences[i][j+2]))[0][1]
-                before_1 = pos_tag(word_tokenize(sentences[i][j-1]))[0][1]
-                before_2 = pos_tag(word_tokenize(sentences[i][j-2]))[0][1]   
+                if (j == 0): 
+                    before_1 = pos_tag(word_tokenize(sentences[i-1][len(sentences[i-1])-1]))[0][1]
+                    before_2 = pos_tag(word_tokenize(sentences[i-1][len(sentences[i-1])-2]))[0][1]
+                    after_1 = pos_tag(word_tokenize(sentences[i][j+1]))[0][1]
+                    after_2 = pos_tag(word_tokenize(sentences[i][j+2]))[0][1]
 
-                word_after_1 = sentences[i][j+1]
-                word_after_2 = sentences[i][j+2]
-                word_before_1 = sentences[i][j-1]
-                word_before_2 = sentences[i][j-2]
-        elif (i == len(sentences)-1 and j == len(sentences[i])-1):
-            before_1 = pos_tag(word_tokenize(sentences[i][j-1]))[0][1]
-            before_2 = pos_tag(word_tokenize(sentences[i][j-2]))[0][1]
+                    before_word_1 = sentences[i-1][len(sentences[i-1])-1]
+                    before_word_2 = sentences[i-1][len(sentences[i-1])-2]
+                    after_word_1 = sentences[i][j+1]
+                    after_word_2 = sentences[i][j+2]
+                elif (j == 1):
+                    before_1 = pos_tag(word_tokenize(sentences[i][0]))[0][1]
+                    before_2 = pos_tag(word_tokenize(sentences[i-1][len(sentences[i-1])-1]))[0][1]
+                    after_1 = pos_tag(word_tokenize(sentences[i][j+1]))[0][1]
+                    after_2 = pos_tag(word_tokenize(sentences[i][j+2]))[0][1]
 
-            word_before_1 = sentences[i][j-1]
-            word_before_2 = sentences[i][j-2]
-        elif (i == len(sentences)-1 and j == len(sentences[i])-2):
-            before_1 = pos_tag(word_tokenize(sentences[i][j-1]))[0][1]
-            before_2 = pos_tag(word_tokenize(sentences[i][j-2]))[0][1]
-            after_1 = pos_tag(word_tokenize(sentences[i][j+1]))[0][1]
+                    before_word_1 = sentences[i][0]
+                    before_word_2 = sentences[i-1][len(sentences[i-1])-1]
+                    after_word_1 = sentences[i][j+1]
+                    after_word_2 = sentences[i][j+2]
+                elif (j == len(sentences[i])-1):
+                    before_1 = pos_tag(word_tokenize(sentences[i][j-1]))[0][1]
+                    before_2 = pos_tag(word_tokenize(sentences[i][j-2]))[0][1]
+                    after_1 = pos_tag(word_tokenize(sentences[i+1][0]))[0][1]
+                    after_2 = pos_tag(word_tokenize(sentences[i+1][1]))[0][1]
 
-            word_before_1 = sentences[i][j-1]
-            word_before_2 = sentences[i][j-2]
-            word_after_1 = sentences[i][j+1]
-        else: 
-            if (j == 0): 
-                before_1 = pos_tag(word_tokenize(sentences[i-1][len(sentences[i-1])-1]))[0][1]
-                before_2 = pos_tag(word_tokenize(sentences[i-1][len(sentences[i-1])-2]))[0][1]
-                after_1 = pos_tag(word_tokenize(sentences[i][j+1]))[0][1]
-                after_2 = pos_tag(word_tokenize(sentences[i][j+2]))[0][1]
+                    before_word_1 = sentences[i][j-1]
+                    before_word_2 = sentences[i][j-2]
+                    after_word_1 = sentences[i+1][0]
+                    after_word_2 = sentences[i+1][1]
+                elif ( j == len(sentences[i])-2):
+                    before_1 = pos_tag(word_tokenize(sentences[i][j-1]))[0][1]
+                    before_2 = pos_tag(word_tokenize(sentences[i][j-2]))[0][1]
+                    after_1 = pos_tag(word_tokenize(sentences[i][j+1]))[0][1]
+                    after_2 = pos_tag(word_tokenize(sentences[i+1][0]))[0][1]
 
-                before_word_1 = sentences[i-1][len(sentences[i-1])-1]
-                before_word_2 = sentences[i-1][len(sentences[i-1])-2]
-                after_word_1 = sentences[i][j+1]
-                after_word_2 = sentences[i][j+2]
-            elif (j == 1):
-                before_1 = pos_tag(word_tokenize(sentences[i][0]))[0][1]
-                before_2 = pos_tag(word_tokenize(sentences[i-1][len(sentences[i-1])-1]))[0][1]
-                after_1 = pos_tag(word_tokenize(sentences[i][j+1]))[0][1]
-                after_2 = pos_tag(word_tokenize(sentences[i][j+2]))[0][1]
+                    before_word_1 = sentences[i][j-1]
+                    before_word_2 = sentences[i][j-2]
+                    after_word_1 = sentences[i][j+1]
+                    after_word_2 = sentences[i+1][0]
+                else: 
+                    before_1 = pos_tag(word_tokenize(sentences[i][j-1]))[0][1]
+                    before_2 = pos_tag(word_tokenize(sentences[i][j-2]))[0][1]
+                    after_1 = pos_tag(word_tokenize(sentences[i][j+1]))[0][1]
+                    after_2 = pos_tag(word_tokenize(sentences[i][j+2]))[0][1]
 
-                before_word_1 = sentences[i][0]
-                before_word_2 = sentences[i-1][len(sentences[i-1])-1]
-                after_word_1 = sentences[i][j+1]
-                after_word_2 = sentences[i][j+2]
-            elif (j == len(sentences[i])-1):
-                before_1 = pos_tag(word_tokenize(sentences[i][j-1]))[0][1]
-                before_2 = pos_tag(word_tokenize(sentences[i][j-2]))[0][1]
-                after_1 = pos_tag(word_tokenize(sentences[i+1][0]))[0][1]
-                after_2 = pos_tag(word_tokenize(sentences[i+1][1]))[0][1]
+                    before_word_1 = sentences[i][j-1]
+                    before_word_2 = sentences[i][j-2]
+                    after_word_1 = sentences[i][j+1]
+                    after_word_2 = sentences[i][j+2]
 
-                before_word_1 = sentences[i][j-1]
-                before_word_2 = sentences[i][j-2]
-                after_word_1 = sentences[i+1][0]
-                after_word_2 = sentences[i+1][1]
-            elif ( j == len(sentences[i])-2):
-                before_1 = pos_tag(word_tokenize(sentences[i][j-1]))[0][1]
-                before_2 = pos_tag(word_tokenize(sentences[i][j-2]))[0][1]
-                after_1 = pos_tag(word_tokenize(sentences[i][j+1]))[0][1]
-                after_2 = pos_tag(word_tokenize(sentences[i+1][0]))[0][1]
+            is_positive = checkForPositivenessNegativeness(sentences[i][j])[0]
+            is_negative = checkForPositivenessNegativeness(sentences[i][j])[1]  
 
-                before_word_1 = sentences[i][j-1]
-                before_word_2 = sentences[i][j-2]
-                after_word_1 = sentences[i][j+1]
-                after_word_2 = sentences[i+1][0]
-            else: 
-                before_1 = pos_tag(word_tokenize(sentences[i][j-1]))[0][1]
-                before_2 = pos_tag(word_tokenize(sentences[i][j-2]))[0][1]
-                after_1 = pos_tag(word_tokenize(sentences[i][j+1]))[0][1]
-                after_2 = pos_tag(word_tokenize(sentences[i][j+2]))[0][1]
+            is_word_before_1_positive = checkForPositivenessNegativeness(word_before_1)[0]
+            is_word_before_1_negative = checkForPositivenessNegativeness(word_before_1)[1]
 
-                before_word_1 = sentences[i][j-1]
-                before_word_2 = sentences[i][j-2]
-                after_word_1 = sentences[i][j+1]
-                after_word_2 = sentences[i][j+2]
+            is_word_before_2_positive = checkForPositivenessNegativeness(word_before_2)[0]
+            is_word_before_2_negative = checkForPositivenessNegativeness(word_before_2)[1]
 
-        is_positive = checkForPositivenessNegativeness(sentences[i][j])[0]
-        is_negative = checkForPositivenessNegativeness(sentences[i][j])[1]  
+            is_word_after_1_positive = checkForPositivenessNegativeness(word_after_1)[0]
+            is_word_after_1_negative = checkForPositivenessNegativeness(word_after_1)[1]
 
-        is_word_before_1_positive = checkForPositivenessNegativeness(word_before_1)[0]
-        is_word_before_1_negative = checkForPositivenessNegativeness(word_before_1)[1]
-
-        is_word_before_2_positive = checkForPositivenessNegativeness(word_before_2)[0]
-        is_word_before_2_negative = checkForPositivenessNegativeness(word_before_2)[1]
-
-        is_word_after_1_positive = checkForPositivenessNegativeness(word_after_1)[0]
-        is_word_after_1_negative = checkForPositivenessNegativeness(word_after_1)[1]
-
-        is_word_after_2_positive = checkForPositivenessNegativeness(word_after_2)[0]
-        is_word_after_2_negative = checkForPositivenessNegativeness(word_after_2)[1] 
- 
-        myWord = Word(sentences[i][j], word, before_1, before_2, after_1, after_2, is_positive, is_negative, is_word_before_1_positive, is_word_before_1_negative, is_word_before_2_positive, is_word_before_2_negative, is_word_after_1_positive, is_word_after_1_negative, is_word_after_2_positive, is_word_after_2_negative, is_weak, is_strong)
-        print(myWord.positivenessStringify())
-        print(myWord.subjectivenessStringify())
-        sentence.addWord(myWord)
-    allSentences.append(sentence)
-       
-
+            is_word_after_2_positive = checkForPositivenessNegativeness(word_after_2)[0]
+            is_word_after_2_negative = checkForPositivenessNegativeness(word_after_2)[1] 
+    
+            myWord = Word(sentences[i][j], word, before_1, before_2, after_1, after_2, is_positive, is_negative, is_word_before_1_positive, is_word_before_1_negative, is_word_before_2_positive, is_word_before_2_negative, is_word_after_1_positive, is_word_after_1_negative, is_word_after_2_positive, is_word_after_2_negative, is_weak, is_strong)
+            # print(myWord.positivenessStringify())
+            # print(myWord.subjectivenessStringify())
+            sentence.addWord(myWord)
+        allSentences.append(sentence)
+        
+print(isWordConcrete('ABODE'))
