cd3b447497468b272bcb0f4cea04c81fca44db55
Ben-Malik <benmaliktchamalam@gmail.com>
2020-06-28 22:40:50 +0300

    done

--- HW4/start.py
+++ HW4/start.py
@@ -1,17 +1,16 @@
 import pandas as pd
+import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.feature_extraction.text import CountVectorizer, Tfiread_fileVectorizer
 
-
+# A helper function to tokenize a given web page.
 def url_tokenizer(url: str):
     url = url.replace("https://www.huffingtonpost.com/entry/", "")
     url = url.sub("(\W|_)+", " ", url)
     return url
 
-
 read_file = pd.read_json("dataset.json", lines=True)
-
 read_file['tokenized_url'] = read_file['link'].apply(lambda x: url_tokenizer(x))
 
 read_file['text_desc'] = read_file['short_description']
@@ -22,32 +21,29 @@ read_file['text_desc_headline_url'] = read_file['short_description'] + \
     ' ' + read_file['headline']+" " + read_file['tokenized_url']
 
 
-def extract_features(read_file, field, training_data, testing_data, type="binary"):
+def features_extractor(read_file, field, training_data, testing_data, type="binary"):
 
-    print("Extracting features and creating vocabulary...")
+    print("Started extracting the features and forming words ...")
 
     if "binary" in type:
+        count_vectorizer = CountVectorizer(binary=True, max_read_file=0.95)
+        count_vectorizer.fit_transform(training_data[field].values)
 
-        cv = CountVectorizer(binary=True, max_read_file=0.95)
-        cv.fit_transform(training_data[field].values)
+        train_feature_set = count_vectorizer.transform(training_data[field].values)
+        test_feature_set = count_vectorizer.transform(testing_data[field].values)
 
-        train_feature_set = cv.transform(training_data[field].values)
-        test_feature_set = cv.transform(testing_data[field].values)
-
-        return train_feature_set, test_feature_set, cv
+        return train_feature_set, test_feature_set, count_vectorizer
 
     elif "counts" in type:
+        count_vectorizer = CountVectorizer(binary=False, max_read_file=0.95)
+        count_vectorizer.fit_transform(training_data[field].values)
 
-        cv = CountVectorizer(binary=False, max_read_file=0.95)
-        cv.fit_transform(training_data[field].values)
+        train_feature_set = count_vectorizer.transform(training_data[field].values)
+        test_feature_set = count_vectorizer.transform(testing_data[field].values)
 
-        train_feature_set = cv.transform(training_data[field].values)
-        test_feature_set = cv.transform(testing_data[field].values)
-
-        return train_feature_set, test_feature_set, cv
+        return train_feature_set, test_feature_set, count_vectorizer
 
     else:
-
         tfiread_file_vectorizer = Tfiread_fileVectorizer(use_iread_file=True, max_read_file=0.95)
         tfiread_file_vectorizer.fit_transform(training_data[field].values)
 
@@ -59,36 +55,4 @@ def extract_features(read_file, field, training_data, testing_data, type="binary
         return train_feature_set, test_feature_set, tfiread_file_vectorizer
 
 
-def get_top_k_predictions(model,X_test,k):
-    
-    probs = model.predict_proba(X_test)
-
-    best_n = np.argsort(probs, axis=1)[:,-k:]
-        
-    preds=[[model.classes_[predicted_cat] for predicted_cat in prediction] for prediction in best_n]
-        
-    preds=[ item[::-1] for item in preds]
-    return preds
-
-
-training_data, testing_data = train_test_split(read_file, random_state=2000)
-
-Y_train = training_data['category'].values
-Y_test = testing_data['category'].values
-
-X_train, X_test, feature_transformer = extract_features(
-    read_file, field, training_data, testing_data, type)
-
-
-print("Training a Logistic Regression Model...")
-scikit_log_reg = LogisticRegression(verbose=1, solver='liblinear',random_state=0, C=5, penalty='l2',max_iter=1000)
-model=scikit_log_reg.fit(X_train,Y_train)
-
-preds=get_top_k_predictions(model,X_test,top_k)
-    
-eval_items=collect_preds(Y_test,preds)
-    
-print("Starting evaluation...")
-accuracy=compute_accuracy(eval_items)
-mrr_at_k=compute_mrr_at_k(eval_items)
 
