0add4ba5ffcfcb09a71b9f088867daf992b610b0
Ben-Malik <benmaliktchamalam@gmail.com>
2020-06-28 22:25:51 +0300

    updating...

--- HW4/start.py
+++ HW4/start.py
@@ -1,41 +1,34 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
-from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.feature_extraction.text import CountVectorizer, Tfiread_fileVectorizer
 
 
-def tokenize_url(url: str):
+def url_tokenizer(url: str):
     url = url.replace("https://www.huffingtonpost.com/entry/", "")
-    url = re.sub("(\W|_)+", " ", url)
+    url = url.sub("(\W|_)+", " ", url)
     return url
 
 
-# read dataset
-df = pd.read_json("dataset.json", lines=True)
+read_file = pd.read_json("dataset.json", lines=True)
 
-# create tokenized URL field
-df['tokenized_url'] = df['link'].apply(lambda x: tokenize_url(x))
+read_file['tokenized_url'] = read_file['link'].apply(lambda x: url_tokenizer(x))
 
-# just the description
-df['text_desc'] = df['short_description']
+read_file['text_desc'] = read_file['short_description']
 
-#description + headline
-df['text_desc_headline'] = df['short_description'] + ' ' + df['headline']
+read_file['text_desc_headline'] = read_file['short_description'] + ' ' + read_file['headline']
 
-# description + tokenized url
-df['text_desc_headline_url'] = df['short_description'] + \
-    ' ' + df['headline']+" " + df['tokenized_url']
+read_file['text_desc_headline_url'] = read_file['short_description'] + \
+    ' ' + read_file['headline']+" " + read_file['tokenized_url']
 
 
-def extract_features(df, field, training_data, testing_data, type="binary"):
-    """Extract features using different methods"""
+def extract_features(read_file, field, training_data, testing_data, type="binary"):
 
-    logging.info("Extracting features and creating vocabulary...")
+    print("Extracting features and creating vocabulary...")
 
     if "binary" in type:
 
-        # BINARY FEATURE REPRESENTATION
-        cv = CountVectorizer(binary=True, max_df=0.95)
+        cv = CountVectorizer(binary=True, max_read_file=0.95)
         cv.fit_transform(training_data[field].values)
 
         train_feature_set = cv.transform(training_data[field].values)
@@ -45,8 +38,7 @@ def extract_features(df, field, training_data, testing_data, type="binary"):
 
     elif "counts" in type:
 
-        # COUNT BASED FEATURE REPRESENTATION
-        cv = CountVectorizer(binary=False, max_df=0.95)
+        cv = CountVectorizer(binary=False, max_read_file=0.95)
         cv.fit_transform(training_data[field].values)
 
         train_feature_set = cv.transform(training_data[field].values)
@@ -56,59 +48,45 @@ def extract_features(df, field, training_data, testing_data, type="binary"):
 
     else:
 
-        # TF-IDF BASED FEATURE REPRESENTATION
-        tfidf_vectorizer = TfidfVectorizer(use_idf=True, max_df=0.95)
-        tfidf_vectorizer.fit_transform(training_data[field].values)
+        tfiread_file_vectorizer = Tfiread_fileVectorizer(use_iread_file=True, max_read_file=0.95)
+        tfiread_file_vectorizer.fit_transform(training_data[field].values)
 
-        train_feature_set = tfidf_vectorizer.transform(
+        train_feature_set = tfiread_file_vectorizer.transform(
             training_data[field].values)
-        test_feature_set = tfidf_vectorizer.transform(
+        test_feature_set = tfiread_file_vectorizer.transform(
             testing_data[field].values)
 
-        return train_feature_set, test_feature_set, tfidf_vectorizer
+        return train_feature_set, test_feature_set, tfiread_file_vectorizer
 
 
-# GET A TRAIN TEST SPLIT (set seed for consistent results)
-training_data, testing_data = train_test_split(df, random_state=2000)
+training_data, testing_data = train_test_split(read_file, random_state=2000)
 
-# GET LABELS
 Y_train = training_data['category'].values
 Y_test = testing_data['category'].values
 
-# GET FEATURES
 X_train, X_test, feature_transformer = extract_features(
-    df, field, training_data, testing_data, type=feature_rep)
+    read_file, field, training_data, testing_data, type=feature_rep)
 
 
-# INIT LOGISTIC REGRESSION CLASSIFIER
-logging.info("Training a Logistic Regression Model...")
+print("Training a Logistic Regression Model...")
 scikit_log_reg = LogisticRegression(verbose=1, solver='liblinear',random_state=0, C=5, penalty='l2',max_iter=1000)
 model=scikit_log_reg.fit(X_train,Y_train)
 
-
-# GET TOP K PREDICTIONS
 preds=get_top_k_predictions(model,X_test,top_k)
     
-# GET PREDICTED VALUES AND GROUND TRUTH INTO A LIST OF LISTS
 eval_items=collect_preds(Y_test,preds)
     
-# GET EVALUATION NUMBERS ON TEST SET -- HOW DID WE DO?
-logging.info("Starting evaluation...")
+print("Starting evaluation...")
 accuracy=compute_accuracy(eval_items)
 mrr_at_k=compute_mrr_at_k(eval_items)
 
 def get_top_k_predictions(model,X_test,k):
     
-    # get probabilities instead of predicted labels, since we want to collect top 3
     probs = model.predict_proba(X_test)
 
-    # GET TOP K PREDICTIONS BY PROB - note these are just index
     best_n = np.argsort(probs, axis=1)[:,-k:]
         
-    # GET CATEGORY OF PREDICTIONS
     preds=[[model.classes_[predicted_cat] for predicted_cat in prediction] for prediction in best_n]
         
-    # REVERSE CATEGORIES - DESCENDING ORDER OF IMPORTANCE
     preds=[ item[::-1] for item in preds]
-        a
     return preds
